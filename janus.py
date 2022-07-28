3d0fa01f60d0192028c13a5a2be659de0a27573b
manesspl <gloom.hea@gmail.com>
2022-07-28 13:50:35 +0200

    functional changes for stylizing usernames

--- janus.py
+++ janus.py
@@ -11,8 +11,9 @@ from pathlib import Path
 @click.option('-n', '--number', default=10, type=int, help='Define the number of generated usernames (Default: 10)')
 @click.option('--low', is_flag=True, default=False, help='Print usernames in lowercase')
 @click.option('--up', is_flag=True, default=False, help='Print usernames in UPPERCASE')
+@click.option('--cap', is_flag=True, default=False, help='Print usernames Capitalized')
 @click.option('--mix', is_flag=True, default=False, help='Print usernames MiXeD')
-def roll_the_dice(short, long, chars, number, low, up, mix):
+def roll_the_dice(short, long, chars, number, low, up, cap, mix):
     """Generate random usernames with JANUS 😶‍🌫️"""
     # Fetch database
     database = open_database()
@@ -31,7 +32,18 @@ def roll_the_dice(short, long, chars, number, low, up, mix):
             depot.append(result)
             length -= 1
         # Start mixing the generated parts of the username
-        username = shake_mixer(depot, low, up, mix)
+        dice_join = random.choice(['', '.', '-', '_'])
+        if low: 
+            username = style_low(depot, dice_join)
+        elif up:
+            username = style_up(depot, dice_join)
+        elif mix:
+            username = style_mixed(depot, dice_join)
+        elif cap:
+            username = style_cap(depot, dice_join)
+        else:
+            username = random.choice([style_low(depot, dice_join), style_up(depot, dice_join), 
+            style_mixed(depot, dice_join), style_cap(depot, dice_join)])
         # Cut the mixed username in an appetizing length
         cut = 15 if short else chars
         dice_cut = random.randint(6, cut)
@@ -44,25 +56,27 @@ def roll_the_dice(short, long, chars, number, low, up, mix):
     # Send generated usernames to print
     print_output(usernames)
 
-def shake_mixer(depot, low, up, mix):
-    # Defines the character with which the parts of the username are joined
-    dice_join = random.choice(['', '.', '-', '_'])
-    # Defines the style of the username (lowercase, uppercase etc.)
-    if low:
-        username = dice_join.join(random.sample(depot, len(depot))).lower()
-    elif mix:
-        username = dice_join.join(random.sample(depot, len(depot)))
-        username_upper = [username[i] for i in range(0, len(username), 2)]
-        username_lower = [username[i] for i in range(1, len(username), 2)]
-        username_updated = []
-        for x, y in zip(username_upper, username_lower):
-            username_updated.append(x.upper())
-            username_updated.append(y)
-        username = ''.join(username_updated)
-    elif up:
-        username = dice_join.join(random.sample(depot, len(depot))).upper()
-    else:
-        username = dice_join.join(random.sample(depot, len(depot)))
+def style_low(depot, dice_join):
+    username = dice_join.join(random.sample(depot, len(depot))).lower()
+    return username
+
+def style_up(depot, dice_join):
+    username = dice_join.join(random.sample(depot, len(depot))).upper()
+    return username
+
+def style_cap(depot, dice_join):
+    username = dice_join.join(random.sample(depot, len(depot)))
+    return username
+
+def style_mixed(depot, dice_join):
+    username = dice_join.join(random.sample(depot, len(depot)))
+    username_upper = [username[i] for i in range(0, len(username), 2)]
+    username_lower = [username[i] for i in range(1, len(username), 2)]
+    username_updated = []
+    for x, y in zip(username_upper, username_lower):
+        username_updated.append(x.upper())
+        username_updated.append(y)
+    username = ''.join(username_updated)
     return username
 
 def open_database():
