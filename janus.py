ca59b8705047a9ac008c50f25c47f3cf6987ffa3
manesspl <gloom.hea@gmail.com>
2022-07-30 11:47:32 +0200

    updates to database and script

--- janus.py
+++ janus.py
@@ -5,12 +5,10 @@ from pathlib import Path
 
 
 @click.command()
-@click.option('-s', '--short', is_flag=True, default=False, help='Generate shorter usernames')
 @click.option('-l', '--long', is_flag=True, default=False, help='Generate longer usernames')
-@click.option('-c', '--chars', default=25, type=int, help='Define max. character length of username (Default: 25)')
 @click.option('-n', '--number', default=10, type=int, help='Define the number of generated usernames (Default: 10)')
 @click.option('--style', default='', help="Choose style of username: 'low' = lowercase, 'up' = UPPERCASE, 'mix' = MiXeD, 'cap' = In Capitals")
-def roll_the_dice(short, long, chars, number, style):
+def roll_the_dice(long, number, style):
     """Generate random usernames with JANUS 😶‍🌫️"""
     # Fetch database
     database = open_database()
@@ -20,46 +18,43 @@ def roll_the_dice(short, long, chars, number, style):
     usernames = []
     usernames_count=number
     for _x in range(0, usernames_count):
-        length = random.choice([2, 3]) if short else random.choice([2, 3, 4])
-        length = random.choice([4, 5]) if long else random.choice([2, 3, 4])
+        length = 3 if long else 2
         depot = []
         while length > 0:
             dice = random.choice(deck)
             result = random.choice(database[dice]).capitalize()
             depot.append(result)
             length -= 1
+        if random.choice([1, 2, 3, 4]) == 3:  
+            depot[-1] = f"{depot[-1] + str(random.randint(1, 99))}"
         # Start mixing the generated parts of the username
-        dice_join = random.choice(['', '.', '-', '_'])
+        dice_join = '-' if style else random.choice(['-', ''])
         username = start_mixing(depot, dice_join, style)
-        cut = 15 if short else chars
-        dice_cut = random.randint(6, cut)
-        if len(username) > dice_cut and username[:dice_cut].endswith(('_', '-', '.')):
-            dice_cut -= 1
-            username = username[0:dice_cut]  
-        else:
-            username = username[0:dice_cut]
         usernames.append(username)
     # Send generated usernames to print
     print_output(usernames)
 
 def start_mixing(depot, dice_join, style):
-    if style == 'low':
-        username = dice_join.join(random.sample(depot, len(depot))).lower()
-    elif style == 'up':
-        username = dice_join.join(random.sample(depot, len(depot))).upper()
-    elif style == 'cap':
+    if dice_join == '':
         username = dice_join.join(random.sample(depot, len(depot)))
-    elif style == 'mix':
-        username = dice_join.join(random.sample(depot, len(depot)))
-        username_upper = [username[i] for i in range(0, len(username), 2)]
-        username_lower = [username[i] for i in range(1, len(username), 2)]
-        username_updated = []
-        for x, y in zip(username_upper, username_lower):
-            username_updated.append(x.upper())
-            username_updated.append(y)
-        username = ''.join(username_updated)
     else:
-        username = start_mixing(depot, dice_join, random.choice(['low', 'up', 'mix', 'cap']))
+        if style == 'low':
+            username = dice_join.join(random.sample(depot, len(depot))).lower()
+        elif style == 'up':
+            username = dice_join.join(random.sample(depot, len(depot))).upper()
+        elif style == 'cap':
+            username = dice_join.join(random.sample(depot, len(depot)))
+        elif style == 'mix':
+            username = dice_join.join(random.sample(depot, len(depot)))
+            username_upper = [username[i] for i in range(0, len(username), 2)]
+            username_lower = [username[i] for i in range(1, len(username), 2)]
+            username_updated = []
+            for x, y in zip(username_upper, username_lower):
+                username_updated.append(x.upper())
+                username_updated.append(y)
+            username = ''.join(username_updated)
+        else:
+            username = start_mixing(depot, dice_join, style='low')
     return username
 
 def open_database():
